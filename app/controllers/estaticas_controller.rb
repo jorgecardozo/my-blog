27a5e07e88339e1a01caeec89ffc6b7f513bc426
Jorge Silvio Cardozo <jorgecardozosilvio@gmail.com>
2021-10-08 19:45:33 -0300

    Add Header and title

--- app/controllers/estaticas_controller.rb
+++ app/controllers/estaticas_controller.rb
@@ -1,10 +1,14 @@
 class EstaticasController < ApplicationController
   def contacto
+    @id     = params[:id].to_i + 10
+    @pagina = params[:pagina]
   end
 
   def nosotros
   end
 
   def otra
+    @message = "Hello from controller"
+    @users = ['Jorge', 'Juan', 'Ester']
   end
 end
